fa57392fedcb5944c2cb868fcbeef96042f917ff
myrel <myrel.br@gmail.com>
2021-03-16 18:42:34 -0300

    inserindo as atualizacoes de giulia

--- acoes/conexao.php
+++ acoes/conexao.php
@@ -1,19 +1,10 @@
 <?php
     Class Conexao{
-        
+        /*
         private $server = "127.0.0.1";
         private $usuario = "root";
         private $senha = "";
         private $banco = "festabatata";
-<<<<<<< HEAD
-       
-         /*
-        private $server = "127.0.0.1";
-        private $usuario = "batata";
-        private $senha = "f3st@b@t123";
-        private $banco = "festabatata";
-*/
-=======
         private $porta = "3309";
         */
         private $server = "localhost";
@@ -22,7 +13,6 @@
         private $banco = "festabatata";
         private $porta = "3306";
         
->>>>>>> 606d963a7181a68e1d5330eeb99858c8dff0baa2
         public function conectar(){
             try{
                 $conexao = new PDO("mysql:host=$this->server;dbname=$this->banco;port=$this->porta;", $this->usuario, $this->senha);
