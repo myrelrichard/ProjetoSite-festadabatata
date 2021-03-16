b774d94f6b6acaa669eb1f76be3684b8588226b1
myrel <myrel.br@gmail.com>
2021-03-16 15:50:06 -0300

    atualizando

--- acoes/conexao.php
+++ acoes/conexao.php
@@ -1,16 +1,17 @@
 <?php
     Class Conexao{
-        /*
+        
         private $server = "127.0.0.1";
         private $usuario = "root";
         private $senha = "";
         private $banco = "festabatata";
-        */
+       
+         /*
         private $server = "127.0.0.1";
         private $usuario = "batata";
         private $senha = "f3st@b@t123";
         private $banco = "festabatata";
-
+*/
         public function conectar(){
             try{
                 $conexao = new PDO("mysql:host=$this->server;dbname=$this->banco", $this->usuario, $this->senha);
